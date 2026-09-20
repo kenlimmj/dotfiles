9fc86c8bb14a6545845ac0315a8ca3518b6be7d2
Kenneth Lim Mingjie <kenlimmj@users.noreply.github.com>
2026-09-20 13:24:25 -0400

    refactor(fish): deduplicate _backup_cmd and _ub_cmd in 10-mac.fish

--- fish/conf.d/10-mac.fish
+++ fish/conf.d/10-mac.fish
@@ -1,6 +1,12 @@
 if status is-interactive
-  abbr --add backup "rclone sync -PLu --exclude '.DS_Store' /Users/kenlimmj/Library/CloudStorage/GoogleDrive-kenlimmj@gmail.com/My\ Drive backup:Herring-Crunchy-Wavy-Narrow"
-  abbr --add ub "brew update && brew upgrade --no-ask && brew upgrade --cask --no-ask && brew cleanup && rclone sync -PLu --exclude '.DS_Store' /Users/kenlimmj/Library/CloudStorage/GoogleDrive-kenlimmj@gmail.com/My\ Drive backup:Herring-Crunchy-Wavy-Narrow"
+  set -g _backup_cmd "rclone sync -PLu --exclude '.DS_Store' /Users/kenlimmj/Library/CloudStorage/GoogleDrive-kenlimmj@gmail.com/My\ Drive backup:Herring-Crunchy-Wavy-Narrow"
+  set -g _ub_cmd "brew update && brew upgrade --no-ask && brew upgrade --cask --no-ask && brew cleanup && $_backup_cmd"
+
+  alias backup "$_backup_cmd"
+  alias ub "$_ub_cmd"
+
+  abbr --add backup "$_backup_cmd"
+  abbr --add ub "$_ub_cmd"
   abbr --add cat "bat"
   abbr --add nano "/opt/homebrew/bin/nano"
   abbr --add patch-drm "cp -R /Applications/Google\ Chrome.app/Contents/Frameworks/Google\ Chrome\ Framework.framework/Libraries/WidevineCdm /Applications/Chromium.app/Contents/Frameworks/Chromium\ Framework.framework/Libraries/WidevineCdm"
