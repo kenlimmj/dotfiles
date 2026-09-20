9d7e18643b5f60ac5dc86488a32c8fc6c0a7ac9d
Kenneth Lim Mingjie <kenlimmj@users.noreply.github.com>
2026-09-20 13:23:14 -0400

    feat(fish): merge rclone backup into ub abbreviation in 10-mac.fish

--- fish/conf.d/10-mac.fish
+++ fish/conf.d/10-mac.fish
@@ -1,5 +1,6 @@
 if status is-interactive
-  abbr --add ub "brew update && brew upgrade && brew upgrade --cask && brew cleanup"
+  abbr --add backup "rclone sync -PLu --exclude '.DS_Store' /Users/kenlimmj/Library/CloudStorage/GoogleDrive-kenlimmj@gmail.com/My\ Drive backup:Herring-Crunchy-Wavy-Narrow"
+  abbr --add ub "brew update && brew upgrade --no-ask && brew upgrade --cask --no-ask && brew cleanup && rclone sync -PLu --exclude '.DS_Store' /Users/kenlimmj/Library/CloudStorage/GoogleDrive-kenlimmj@gmail.com/My\ Drive backup:Herring-Crunchy-Wavy-Narrow"
   abbr --add cat "bat"
   abbr --add nano "/opt/homebrew/bin/nano"
   abbr --add patch-drm "cp -R /Applications/Google\ Chrome.app/Contents/Frameworks/Google\ Chrome\ Framework.framework/Libraries/WidevineCdm /Applications/Chromium.app/Contents/Frameworks/Chromium\ Framework.framework/Libraries/WidevineCdm"
